fe5e5d631ffe71dacaed025b37195df5b11edfaf
Harshit Prasad <harshitprasad532@gmail.com>
2024-09-09 08:35:55 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +9,28 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  http = inject(HttpClient)
+
   title = 'client';
+
+  users: any[] = []
+
+  ngOnInit(): void {
+    this.http
+      .get('http://localhost:5000/api/User')
+      .subscribe({
+        next: (response: any) => {
+          console.log(response);
+          this.users = response;
+        },
+        error: (error) => {
+          console.log(error);
+        },
+        complete: () => {
+          console.log('Request completed');
+        }
+      });
+  }
+
 }
